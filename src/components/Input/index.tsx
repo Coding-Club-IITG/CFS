0172ceb237be54b479fe2f2751f62c512dcf85d3
AmreshSinha <35039730+AmreshSinha@users.noreply.github.com>
2022-07-31 16:54:49 +0530

    Trying stuff

---
+++ src/components/Input/index.tsx
@@ -0,0 +1,13 @@
+import styled from 'styled-components'
+
+export default function Input() {
+  return (
+    <>
+      <InputBox />
+    </>
+  )
+}
+
+const InputBox = styled.input`
+  color: #f9f5eb;
+`
