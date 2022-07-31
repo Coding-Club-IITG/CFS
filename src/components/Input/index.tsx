6a575ffd901adad406137639b627837501e0a803
AmreshSinha <35039730+AmreshSinha@users.noreply.github.com>
2022-07-31 17:17:34 +0530

    Add Input component

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -3,11 +3,23 @@ import styled from 'styled-components';
 export default function Input() {
   return (
     <>
-      <InputBox />
+      <InputBox type="text" placeholder="Enter Code" />
     </>
   );
 }
 
 const InputBox = styled.input`
-  color: #f9f5eb;
+  width: 12rem;
+  border: 2px solid #f9f5eb;
+  border-radius: 4px;
+  margin: 8px 0;
+  outline: none;
+  padding: 1rem;
+  box-sizing: border-box;
+  transition: 0.3s;
+
+  :focus {
+    border-color: dodgerBlue;
+    box-shadow: 0 0 8px 0 dodgerBlue;
+  }
 `;
